7c11a9ba8e8bfe09166b669b49ceba46960002bc
Sergey <sergey1888888@gmail.com>
2021-04-04 19:44:32 +0300

    fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import MapGL from "react-map-gl";
 
+const token = "pk.eyJ1IjoiZm9sbGV4IiwiYSI6ImNrZjN0MXBqYTA2YzAyem5vYWh0OXJ4eWcifQ.T10EKgjFamd6JnCRJr4_Ow"
+
 const Map = () => {
 	const [viewport, setViewport] = useState({
 		latitude: 48.707103,
@@ -16,7 +18,7 @@ const Map = () => {
 			height="100%"
 			mapStyle="mapbox://styles/follex/ckm6poqh3cjbd17o5gtpfc6u3"
 			onViewportChange={setViewport}
-			mapboxApiAccessToken="pk.eyJ1IjoiZm9sbGV4IiwiYSI6ImNrZjN0MXBqYTA2YzAyem5vYWh0OXJ4eWcifQ.T10EKgjFamd6JnCRJr4_Ow"
+			mapboxApiAccessToken={token}
 		/>
 	);
 };
